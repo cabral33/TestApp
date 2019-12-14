7ee174fe267c10472fab8769fcabd65a1d0ed8c5
lucas simplicio <lsc.hef@uea.edu.br>
2019-12-14 12:21:49 -0400

    ignore

--- app/src/main/java/livro/com/br/MainActivity.java
+++ app/src/main/java/livro/com/br/MainActivity.java
@@ -1,7 +1,14 @@
 package livro.com.br;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +17,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//        Uri site = Uri.parse("android.com.br");
+//        Intent intent = new Intent(Intent.ACTION_VIEW, site);
+//        startActivity(intent);
 
     }
 }
